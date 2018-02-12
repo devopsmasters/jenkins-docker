ea5b0a40b926addb628ce0d81265dc0b2e31d437
devopsmasters <33680369+devopsmasters@users.noreply.github.com>
2018-02-12 08:33:19 +0530

    Update maven.groovy

--- groovy/maven.groovy
+++ groovy/maven.groovy
@@ -1,18 +1,19 @@
-import hudson.tasks.Maven
-import hudson.tasks.Maven.MavenInstallation;
-import hudson.tools.InstallSourceProperty;
-import hudson.tools.ToolProperty;
-import hudson.tools.ToolPropertyDescriptor
-import hudson.tools.ZipExtractionInstaller;
-import hudson.util.DescribableList
-import jenkins.model.Jenkins;
-def extensions = Jenkins.instance.getExtensionList(Maven.DescriptorImpl.class)[0]
-List<MavenInstallation> installations = []
-mavenToool = ['name': 'maven3', 'url': 'file:/var/jenkins_home/downloads/apache-maven-3.5.0-bin.tar.gz', 'subdir': 'apache-maven-3.5.0']
-println("Setting up tool: ${mavenToool.name} ")
-def describableList = new DescribableList<ToolProperty<?>, ToolPropertyDescriptor>()
-def installer = new ZipExtractionInstaller(mavenToool.label as String, mavenToool.url as String, mavenToool.subdir as String);
-describableList.add(new InstallSourceProperty([installer]))
-installations.add(new MavenInstallation(mavenToool.name as String, "", describableList))
-extensions.setInstallations(installations.toArray(new MavenInstallation[installations.size()]))
-extensions.save()
+import com.cloudbees.jenkins.plugins.sshcredentials.impl.*
+import com.cloudbees.plugins.credentials.*
+import com.cloudbees.plugins.credentials.common.*
+import com.cloudbees.plugins.credentials.domains.*
+import com.cloudbees.plugins.credentials.impl.*
+import groovy.transform.InheritConstructors
+import hudson.model.*
+import hudson.plugins.sshslaves.*
+import hudson.util.*
+import jenkins.model.*
+import jenkins.security.*
+import org.apache.commons.io.IOUtils
+import org.jenkinsci.plugins.*
+    
+    def inst = Jenkins.getInstance()    
+    def desc = inst.getDescriptor("hudson.tasks.Maven")
+    def minst =  new hudson.tasks.Maven.MavenInstallation('maven3', '/opt/maven');
+    desc.setInstallations(minst)
+    desc.save()
