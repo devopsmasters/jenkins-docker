1f6b7dffbecc5532251a738b958f6898d9f546ee
devopsmasters <33680369+devopsmasters@users.noreply.github.com>
2018-02-17 01:15:38 +0530

    Update users.groovy

--- groovy/users.groovy
+++ groovy/users.groovy
@@ -3,7 +3,10 @@ import hudson.security.*
 
 def instance = Jenkins.getInstance()
 
+def env_user = System.getenv('MASTERUSER')
+def env_passwd = System.getenv('MASTERPWD')
+
 def hudsonRealm = new HudsonPrivateSecurityRealm(false)
- hudsonRealm.createAccount('admin','admin!23')
+ hudsonRealm.createAccount("$env_user","$env_passwd")
 instance.setSecurityRealm(hudsonRealm)
 instance.save()
