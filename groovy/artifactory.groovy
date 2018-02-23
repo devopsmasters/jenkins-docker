67839a9be144604a55d3d1b3e02974b6018ce8b5
devopsmasters <33680369+devopsmasters@users.noreply.github.com>
2018-02-24 04:07:57 +0530

    Update artifactory.groovy

--- groovy/artifactory.groovy
+++ groovy/artifactory.groovy
@@ -6,11 +6,12 @@ import org.jfrog.hudson.*
 import com.cloudbees.plugins.credentials.CredentialsProvider
 import com.cloudbees.plugins.credentials.common.StandardUsernameCredentials
 
+    def env_arturl = System.getenv('ARTIFACTORY_URL')
     def inst = Jenkins.getInstance()
     def desc = inst.getDescriptor("org.jfrog.hudson.ArtifactoryBuilder")
     CredentialsConfig deployerCredentials = new CredentialsConfig('admin', 'password', 'artifactory-user',false)
     List<ArtifactoryServer> servers =  desc.getArtifactoryServers()
-    ArtifactoryServer server = new ArtifactoryServer('local-artifactory', 'http://localhost:8081/artifactory', deployerCredentials, null, 300, false, 3)
+ArtifactoryServer server = new ArtifactoryServer('local-artifactory', "${env_arturl}", deployerCredentials, null, 300, false, 3)
     if (servers == null || servers.empty) {
       servers = [server]
     } else {
